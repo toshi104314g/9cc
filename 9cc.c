6b3ac6834782ef1e2ca14678a137b7be6beadc2b
toshiyuki <toshiyuki@gitlab24>
2022-05-20 16:07:13 +0900

    lenをtokenに追加

--- 9cc.c
+++ 9cc.c
@@ -20,6 +20,7 @@ struct Token {
   Token *next;    // 次の入力トークン
   int val;        // kindがTK_NUMの場合、その数値
   char *str;      // トークン文字列
+  int len;	  // トークンの長さ
 };
 
 // 現在着目しているトークン
@@ -44,8 +45,8 @@ void error_at(char *loc, char *fmt, ...) {
 
 // 次のトークンが期待している記号のときには、トークンを1つ読み進めて
 // 真を返す。それ以外の場合には偽を返す。
-bool consume(char op) {
-  if (token->kind != TK_RESERVED || token->str[0] != op)
+bool consume(char *op) {
+  if (token->kind != TK_RESERVED || strlen(op) != token->len || memcmp(token->str, op, token->len))
     return false;
   token = token->next;
   return true;
@@ -53,8 +54,8 @@ bool consume(char op) {
 
 // 次のトークンが期待している記号のときには、トークンを1つ読み進める。
 // それ以外の場合にはエラーを報告する。
-void expect(char op) {
-  if (token->kind != TK_RESERVED || token->str[0] != op)
+void expect(char *op) {
+  if (token->kind != TK_RESERVED || strlen(op) != token->len || memcmp(token->str, op, token->len))
     error_at(token->str,"'%c'ではありません", op);
   token = token->next;
 }
@@ -98,6 +99,7 @@ Token *tokenize(char *p) {
     if (*p == '+' || *p == '-' || *p == '*' || *p == '/'
 	|| *p == '(' || *p == ')') {
       cur = new_token(TK_RESERVED, cur, p++);
+      cur->len = 1;
       continue;
     }
 
@@ -157,9 +159,9 @@ Node *expr() {
   Node *node = mul();
 
   for (;;) {
-    if (consume('+'))
+    if (consume((char*)memset(malloc(sizeof(char)),'+',1)))
       node = new_node(ND_ADD, node, mul());
-    else if (consume('-'))
+    else if (consume((char*)memset(malloc(sizeof(char)),'-',1)))
       node = new_node(ND_SUB, node, mul());
     else
       return node;
@@ -171,9 +173,9 @@ Node *mul() {
   Node *node = unary();
 
   for (;;) {
-    if (consume('*'))
+    if (consume((char*)memset(malloc(sizeof(char)),'*',1)))
       node = new_node(ND_MUL, node, unary());
-    else if (consume('/'))
+    else if (consume((char*)memset(malloc(sizeof(char)),'/',1)))
       node = new_node(ND_DIV, node, unary());
     else
       return node;
@@ -182,18 +184,18 @@ Node *mul() {
 
 
 Node *unary() {
-  if (consume('+'))
+  if (consume((char*)memset(malloc(sizeof(char)),'+',1)))
     return primary();
-  if (consume('-'))
+  if (consume((char*)memset(malloc(sizeof(char)),'-',1)))
     return new_node(ND_SUB, new_node_num(0), primary());
   return primary();
 }
 
 Node *primary() {
   // 次のトークンが"("なら、"(" expr ")"のはず
-  if (consume('(')) {
+  if (consume((char*)memset(malloc(sizeof(char)),'(',1))) {
     Node *node = expr();
-    expect(')');
+    expect((char*)memset(malloc(sizeof(char)),')',1));
     return node;
   }
 
