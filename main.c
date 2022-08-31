2e3480999d2dc949faa03099d4a9d3fe0dca7c0f
toshiyuki <toshiyuki@gitlab24>
2022-08-31 12:28:29 +0900

    ファイルを分割コンパイルに変更

---
+++ main.c
@@ -0,0 +1,49 @@
+
+#include "9cc.h"
+
+// 現在着目しているトークン
+Token *token;
+
+// 入力プログラム
+char *user_input;
+
+// エラー箇所を報告する
+void error_at(char *loc, char *fmt, ...) {
+  va_list ap;
+  va_start(ap, fmt);
+
+  int pos = loc - user_input;
+  fprintf(stderr, "%s\n", user_input);
+  fprintf(stderr, "%*s", pos, " "); // pos個の空白を出力
+  fprintf(stderr, "^ ");
+  vfprintf(stderr, fmt, ap);
+  fprintf(stderr, "\n");
+  exit(1);
+}
+
+int main(int argc, char **argv) {
+  if (argc != 2) {
+    error_at(token->str,"引数の個数が正しくありません");
+    return 1;
+  }
+
+  // トークナイズしてパースする
+  user_input = argv[1];
+  token = tokenize(user_input);
+  Node *node = expr();
+
+  // アセンブリの前半部分を出力
+  printf(".intel_syntax noprefix\n");
+  printf(".globl main\n");
+  printf("main:\n");
+
+  // 抽象構文木を下りながらコード生成
+  gen(node);
+
+  // スタックトップに式全体の値が残っているはずなので
+  // それをRAXにロードして関数からの返り値とする
+  printf("  pop rax\n");
+  printf("  ret\n");
+  return 0;
+}
+
